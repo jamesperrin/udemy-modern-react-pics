830e9371f656b9a3af797f5d71d3cd963cd33491
James Perrin <countrydawgg@gmail.com>
2020-07-09 16:14:39 -0700

    Add onInputChange draft

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    onInputChange(evt){
+        console.log(evt.target.value);
+    }
+
     render(){
         return (
         <div className="ui segment">
             <form className="ui form">
                 <label htmlFor="search" className="ui label">Image Search</label>
-                <input type="text" className="field" id="search" name="search"></input>
+                <input type="text" className="field" id="search" name="search" onChange={this.onInputChange}></input>
             </form>
         </div>
         );
