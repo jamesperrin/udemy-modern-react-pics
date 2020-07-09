9d9dac0987c6f01788d519c8132a98de630901af
James Perrin <countrydawgg@gmail.com>
2020-07-08 17:39:28 -0700

    Add Semantic UI URL: https://cdnjs.com/libraries/semantic-ui

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,9 +3,10 @@ import React from 'react';
 class SearchBar extends React.Component {
     render(){
         return (
-        <div>
-            <form>
-                <input type="text"></input>
+        <div className="ui segment">
+            <form className="ui form">
+                <label htmlFor="search" className="ui label">Image Search</label>
+                <input type="text" className="field" id="search" name="search"></input>
             </form>
         </div>
         );
