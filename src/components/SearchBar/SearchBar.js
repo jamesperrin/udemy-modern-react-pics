8cd57af4ab0ac2cd150e1477158578674a985838
James Perrin <countrydawgg@gmail.com>
2020-07-09 16:40:09 -0700

    Refactor SearchBar.js to a controlled component.
    Refactored SearchBar component to a controlled component utilizing state.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    onInputChange(evt){
-        console.log(evt.target.value);
-    }
+    state = { term: '' };
 
-    render(){
+    render() {
         return (
-        <div className="ui segment">
-            <form className="ui form">
-                <label htmlFor="search" className="ui label">Image Search</label>
-                <input type="text" className="field" id="search" name="search" onChange={this.onInputChange}></input>
-            </form>
-        </div>
+            <div className="ui segment">
+                <form className="ui form">
+                    <label htmlFor="SearchTerm" className="ui label">Image Search</label>
+                    <input type="text" className="field" id="SearchTerm" name="SearchTerm"
+                        value={this.state.term} onChange={(e) => this.setState({ term: e.target.value })}></input>
+                </form>
+            </div>
         );
     }
 }
