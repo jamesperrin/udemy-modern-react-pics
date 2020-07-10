d8e5e775cf2a843a2100ada20b1e5b8d135555c1
James Perrin <countrydawgg@gmail.com>
2020-07-09 17:02:36 -0700

    Add onFormSubmit() to handle form submit

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,10 +3,15 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = { term: '' };
 
+    onFormSubmit = (evt) =>{
+        evt.preventDefault();
+        console.log(this.state.term);
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <label htmlFor="SearchTerm" className="ui label">Image Search</label>
                     <input type="text" className="field" id="SearchTerm" name="SearchTerm"
                         value={this.state.term} onChange={(e) => this.setState({ term: e.target.value })}></input>
