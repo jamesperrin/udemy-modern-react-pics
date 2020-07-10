f9b3da24e34d1f0a88bf713440f5e10451312f75
James Perrin <countrydawgg@gmail.com>
2020-07-09 17:09:34 -0700

    Add onSearchSubmit() to App.js.
    1. Add onSearchSubmit to App.js.
    2. Add onSubmit as a prop to SearchBar component.
    3. Completed Section 7: Handling User Input with Forms and Events.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import SearchBar from './SearchBar/SearchBar';
+import React from "react";
+import SearchBar from "./SearchBar/SearchBar";
 
-const App = () => {
+class App extends React.Component {
+    onSearchSubmit(term){
+        console.log(term);
+    }
+  render() {
     return (
-        <div className="ui container" style={{marginTop: '10px'}}>
-            <SearchBar />
-        </div>
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
     );
-};
+  }
+}
 
 export default App;
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React from "react";
 
 class SearchBar extends React.Component {
-    state = { term: '' };
+  state = { term: "" };
 
-    onFormSubmit = (evt) =>{
-        evt.preventDefault();
-        console.log(this.state.term);
-    }
+  onFormSubmit = (evt) => {
+    evt.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
 
-    render() {
-        return (
-            <div className="ui segment">
-                <form className="ui form" onSubmit={this.onFormSubmit}>
-                    <label htmlFor="SearchTerm" className="ui label">Image Search</label>
-                    <input type="text" className="field" id="SearchTerm" name="SearchTerm"
-                        value={this.state.term} onChange={(e) => this.setState({ term: e.target.value })}></input>
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <label htmlFor="SearchTerm" className="ui label">
+            Image Search
+          </label>
+          <input
+            type="text"
+            className="field"
+            id="SearchTerm"
+            name="SearchTerm"
+            value={this.state.term}
+            onChange={(e) => this.setState({ term: e.target.value })}
+          ></input>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
