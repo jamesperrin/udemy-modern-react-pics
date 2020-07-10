f830590f4478631ba3de5bb7e21965a22af7ae61
James Perrin <countrydawgg@gmail.com>
2020-07-09 18:08:56 -0700

    Add Axios package.
    1. Installed Axios package.
    2. An Async Await to Unsplash API.

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,48 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar/SearchBar";
 
 class App extends React.Component {
-    onSearchSubmit(term){
+    async onSearchSubmit(term) {
         console.log(term);
+
+        // Using ASYC AWAIT
+        const response = await axios
+            .get("https://api.unsplash.com/search/photos", {
+                params: {
+                    query: term,
+                },
+                headers: {
+                    Authorization:
+                        "Client-ID jwwfeW0mbqFEVb1SIWHbQRYMhAgcb0YL4pTqkWdG-Tk",
+                },
+            });
+
+        console.log(response.data.results);
+
+        // Using Promises
+        //   axios
+        //   .get("https://api.unsplash.com/search/photos", {
+        //     params: {
+        //       query: term,
+        //     },
+        //     headers: {
+        //       Authorization:
+        //         "Client-ID jwwfeW0mbqFEVb1SIWHbQRYMhAgcb0YL4pTqkWdG-Tk",
+        //     },
+        //   })
+        //   .then((response) => {
+        //     console.log(response.data.results);
+        //   });
     }
-  render() {
-    return (
-      <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <div className="ui container" style={{ marginTop: "10px" }}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    }
 }
 
 export default App;
